0ee5a69aef49d9dece5f783f9ac775bbea51c815
anshu272003 <139258380+anshu272003@users.noreply.github.com>
2024-12-27 22:18:10 +0530

    small change

--- index.js
+++ index.js
@@ -23,8 +23,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:["http://localhost:3000",
+			"process.env.CLIENT_URI,",
+		],
 		credentials:true,
+		
 	})
 )
 
